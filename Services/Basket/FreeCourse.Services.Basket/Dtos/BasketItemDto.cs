9fa14a02b13840e5b0921f1ff944463e4fd31fe7
Sezer YILDIRIM <sezer.yildirim@live.com>
2021-06-03 13:17:18 +0300

    Builded Basket Microservices

---
+++ Services/Basket/FreeCourse.Services.Basket/Dtos/BasketItemDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Basket.Dtos
+{
+    public class BasketItemDto
+    {
+        public int Quantity { get; set; }
+        public int CourseId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
